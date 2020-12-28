3aac433830010548658d51b0350a310764b9b39f
kjmbaldwin <kylejmbaldwin@gmail.com>
2020-12-28 15:54:25 -0500

    refactored

--- app/screens/ViewImageScreen.js
+++ app/screens/ViewImageScreen.js
@@ -1,55 +1,48 @@
 import React from 'react';
 import { Image, View, StyleSheet } from "react-native";
 
+import colors from '../config/colors';
+
 
 const ViewImageScreen = (image) => {
   return(
     <View style={styles.container}>
-      <View style={styles.buttonContainer}>
-        <View style={styles.closeButton}/>
-        <View style={styles.deleteButton}/>
-      </View>
-      <View style={styles.productContainer}>
-        <Image
-          style={styles.product}
-          source={require('../assets/chair.jpg')}
-          resizeMode={'contain'}
-        />
-      </View>
+      <View style={styles.closeButton}/>
+      <View style={styles.deleteButton}/>
+      <Image
+        style={styles.product}
+        source={require('../assets/chair.jpg')}
+        resizeMode={'contain'}
+      />
     </View>
 );
 }
 
 const styles = StyleSheet.create({
+  closeButton: {
+    height: 50,
+    width: 50,
+    position: 'absolute',
+    top: 40,
+    left: 30,
+    backgroundColor: colors.primary
+  },
   container: {
     flex: 1,
-    backgroundColor: 'black',
+    backgroundColor: colors.black,
   },
-  buttonContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between'
-  },
-  productContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+  deleteButton: {
+    height: 50,
+    width: 50,
+    position: 'absolute',
+    top: 40,
+    right: 30,
+    backgroundColor: colors.secondary
   },
   product: {
     width: '100%',
     height: '100%'
   },
-  closeButton: {
-    height: 50,
-    width: 50,
-    left: 30,
-    backgroundColor: '#fc5c65'
-  },
-  deleteButton: {
-    height: 50,
-    width: 50,
-    right: 30,
-    backgroundColor: '#4ECDC4'
-  }
 });
 
 export default ViewImageScreen
--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { ImageBackground, View, StyleSheet, Image, Text } from 'react-native';
 
+import colors from '../config/colors'
+
 const bgImage = require('../assets/background.jpg')
 
 const WelcomeScreen = () => {
@@ -37,12 +39,12 @@ const styles = StyleSheet.create({
   loginButton: {
     width: '100%',
     height: 70,
-    backgroundColor: '#fc5c65'
+    backgroundColor: colors.primary
   },
   registerButton: {
     width: '100%',
     height: 70,
-    backgroundColor: '#4ECDC4'
+    backgroundColor: colors.secondary
   }
 });
 
